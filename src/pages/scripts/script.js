23782f5b5d2b0cd16bb2e4af1a2be92047684a28
magnus madsen <magn@stud.kea.dk>
2023-03-16 11:06:31 +0100

    final navbar ish

--- src/pages/scripts/script.js
+++ src/pages/scripts/script.js
@@ -6,11 +6,14 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   navMenu.classList.toggle("active");
   navLink.classList.toggle("active");
+  document.querySelector("header").classList.toggle("active");
+  document.querySelector(".logo").classList.toggle("active");
 });
-document.querySelectorAll(".nav-child").forEach((n) =>
+document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
     burger.classList.remove("active");
     navMenu.classList.remove("active");
     navLink.classList.remove("active");
+    document.querySelector(".logo").classList.remove("active");
   })
 );
